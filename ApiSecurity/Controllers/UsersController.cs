ada47b2309968ea0cfdeeee437711f2886c75b0f
Nate D. D'Angelo <69875151+nathandangelo1@users.noreply.github.com>
2024-03-03 12:24:46 -0600

    Add project files.

---
+++ ApiSecurity/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiSecurity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public UsersController(IConfiguration config)
+    {
+        this._config = config;
+    }
+
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return _config.GetConnectionString("Default");
+    }
+
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
